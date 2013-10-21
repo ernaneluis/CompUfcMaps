f3f96120357b1f220c447d5753ab3a5d9c11ae8c
Samuel <sammueln@gmail.com>
2013-10-21 09:23:23 -0300

    diferente markers funcionando

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,13 @@
 class PagesController < ApplicationController
   def index
-  	@json = User.all.to_gmaps4rails
+  	@json = User.all.to_gmaps4rails do |user, marker|
+        if user.name == "john"
+            marker.picture({
+                "picture" => 'assets/pin_1.png',
+                "width" =>  '32',
+                "height" => '32'
+             })
+        end
+    end
   end
 end
