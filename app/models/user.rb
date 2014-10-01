82688358bb2b4a99f17601f34cfaabc08ec295c9
Ernane Luis <ernane.luis@gmail.com>
2014-10-01 18:40:19 -0300

    Login modificado

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@ class User < ActiveRecord::Base
 
    def self.authenticate(email, password)
      user = find_by_email(email)
-     if user.password == password
+     #if user && user.password_hash == BCrypt::Engine.hash_secret(password, nil)
+     if user && BCrypt::Password.new(user.password_hash) == password
        user
      else
        nil
@@ -22,8 +23,9 @@ class User < ActiveRecord::Base
 
    def encrypt_password
      if password.present?
-       self.password_salt = BCrypt::Engine.generate_salt
-       self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+       #self.password_salt = BCrypt::Engine.generate_salt
+       #self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+       self.password_hash = BCrypt::Password.create(password)
      end
    end
 
